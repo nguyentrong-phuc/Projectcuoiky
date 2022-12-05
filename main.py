089479645e2ea075a3e7c8dc00e9163c0d64aa53
nguyentrong-phuc <112928938+nguyentrong-phuc@users.noreply.github.com>
2022-12-05 21:58:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 import streamlit as st
 import pandas as pd
-from playsound import playsound
-import speech_recognition as sr
-from gtts import gTTS
-import os
+import pickle
+import warnings
+
+import numpy as np
 import streamlit as st
-
-
-genre = st.radio(
-    "What's your favorite movie genre",
-    ('Comedy', 'Drama', 'Documentary'))
-
-if genre == 'Comedy':
-    st.write('You selected comedy.')
-else:
-    st.write("You didn't select comedy.")
-
+import pandas as pd
 
 
 

68d0f5a70d21673443304680e218a61eb03b3030
nguyentrong-phuc <112928938+nguyentrong-phuc@users.noreply.github.com>
2022-12-23 19:27:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,115 @@
-import pickle
-import warnings
-
-import numpy as np
+from googletrans import Translator
+from playsound import playsound
+import speech_recognition as sr
+from gtts import gTTS
+import os
 import streamlit as st
+from pydub import AudioSegment
+import time
+import pydub
+from pathlib import Path
+from typing import List
 import pandas as pd
-warnings.filterwarnings("ignore")
+from io import StringIO
+st.title('Auto_Translator App')
+IPtype = st.radio("Input Type",('Microphone', 'Text', 'Audio File'),horizontal=True)
 
+Lang=['Afrikaans', 'Albanian', 'Amharic', 'Arabic', 'Armenian', 'Assamese', 'Aymara', 'Azerbaijani', 'Bambara', 'Basque', 'Belarusian', 'Bengali', 'Bhojpuri', 'Bosnian', 'Bulgarian', 'Catalan', 'Cebuano', 'Chinese (Simplified)', 'Chinese (Traditional)', 'Corsican', 'Croatian', 'Czech', 'Danish', 'Dhivehi', 'Dogri', 'Dutch', 'English', 'Esperanto', 'Estonian', 'Ewe', 'Filipino (Tagalog)', 'Finnish', 'French', 'Frisian', 'Galician', 'Georgian', 'German', 'Greek', 'Guarani', 'Gujarati', 'Haitian Creole', 'Hausa', 'Hawaiian', 'Hebrew', 'Hindi', 'Hmong', 'Hungarian', 'Icelandic', 'Igbo', 'Ilocano', 'Indonesian', 'Irish', 'Italian', 'Japanese', 'Javanese', 'Kannada', 'Kazakh', 'Khmer', 'Kinyarwanda', 'Konkani', 'Korean', 'Krio', 'Kurdish', 'Kurdish (Sorani)', 'Kyrgyz', 'Lao', 'Latin', 'Latvian', 'Lingala', 'Lithuanian', 'Luganda', 'Luxembourgish', 'Macedonian', 'Maithili', 'Malagasy', 'Malay', 'Malayalam', 'Maltese', 'Maori', 'Marathi', 'Meiteilon (Manipuri)', 'Mizo', 'Mongolian', 'Myanmar (Burmese)', 'Nepali', 'Norwegian', 'Nyanja (Chichewa)', 'Odia (Oriya)', 'Oromo', 'Pashto', 'Persian', 'Polish', 'Portuguese (Portugal, Brazil)', 'Punjabi', 'Quechua', 'Romanian', 'Russian', 'Samoan', 'Sanskrit', 'Scots Gaelic', 'Sepedi', 'Serbian', 'Sesotho', 'Shona', 'Sindhi', 'Sinhala (Sinhalese)', 'Slovak', 'Slovenian', 'Somali', 'Spanish', 'Sundanese', 'Swahili', 'Swedish', 'Tagalog (Filipino)', 'Tajik', 'Tamil', 'Tatar', 'Telugu', 'Thai', 'Tigrinya', 'Tsonga', 'Turkish', 'Turkmen', 'Twi (Akan)', 'Ukrainian', 'Urdu', 'Uyghur', 'Uzbek', 'Vietnamese', 'Welsh', 'Xhosa', 'Yiddish', 'Yoruba', 'Zulu']
+Code=['af', 'sq', 'am', 'ar', 'hy', 'as', 'ay', 'az', 'bm', 'eu', 'be', 'bn', 'bho', 'bs', 'bg', 'ca', 'ceb', 'zh', 'zh', 'co', 'hr', 'cs', 'da', 'dv', 'doi', 'nl', 'en', 'eo', 'et', 'ee', 'fil', 'fi', 'fr', 'fy', 'gl', 'ka', 'de', 'el', 'gn', 'gu', 'ht', 'ha', 'haw', 'he', 'hi', 'hmn', 'hu', 'is', 'ig', 'ilo', 'id', 'ga', 'it', 'ja', 'jv', 'kn', 'kk', 'km', 'rw', 'gom', 'ko', 'kri', 'ku', 'ckb', 'ky', 'lo', 'la', 'lv', 'ln', 'lt', 'lg', 'lb', 'mk', 'mai', 'mg', 'ms', 'ml', 'mt', 'mi', 'mr', 'mni-Mtei', 'lus', 'mn', 'my', 'ne', 'no', 'ny', 'or', 'om', 'ps', 'fa', 'pl', 'pt', 'pa', 'qu', 'ro', 'ru', 'sm', 'sa', 'gd', 'nso', 'sr', 'st', 'sn', 'sd', 'si', 'sk', 'sl', 'so', 'es', 'su', 'sw', 'sv', 'tl', 'tg', 'ta', 'tt', 'te', 'th', 'ti', 'ts', 'tr', 'tk', 'ak', 'uk', 'ur', 'ug', 'uz', 'vi', 'cy', 'xh', 'yi', 'yo', 'zu']
 
-st.title('Dự đoán chất lượng rượu vang: ')
-col1, col2, col3 =st.columns(3)
+if IPtype == 'Text':
+    col1,col3 = st.columns(2)
+    with col1:
+        option1 = st.selectbox("Your Language",Lang)
+        in_text = st.text_area("Enter text:",height=8,max_chars=None,key=None,help="Enter your text here")
+        button = st.button('Translate')
+    with col3:
+        option3 = st.selectbox("Target Language",Lang)
+    Yourlang = Code[Lang.index(option1)]
+    Targlang = Code[Lang.index(option3)]
 
-# Id= np.str(col1.text_input("Id"))
-fixed_acidity= np.str(col1.slider('fixed acidity', 0.0, 20.0, 5.0))
-volatile_acidity= np.str(col2.slider('volatile acidity (g/L)', 0.00, 2.00, 0.55))
-citric_acid= np.str(col3.slider('citric acid (g/L)', 0.00, 1.00, 0.55))
-residual_sugar= np.str(col1.slider('residual sugar(g/L)', 0.00, 25.00, 10.00))
-chlorides= np.str(col2.slider('chlorides', 0.000,0.001, 0.2, 0.001, format="%f"))
-free_sulfur_dioxide= np.str(col3.slider('free sulfur dioxide'  ))
-total_sulfur_dioxide= np.str(col1.slider('total sulfur dioxide', 0, 250, 100))
-density= np.str(col2.text_input('density'))
-pH= np.str(col3.slider('pH', 0.00, 5.00, 3.00))
-sulphates= np.str(col1.slider('sulphates', 0.00, 2.00, 1.00))
-alcohol= np.str(col2.slider('alcohol', 0.0, 25.0, 10.0 ))
+    with col3:
+        if button:
+                translator = Translator()
+                translation = translator.translate(in_text, dest=Targlang)
+                trans = translation.text
+                st.text_area('Translated', trans, height=8, max_chars=None, key=None)
+                speak = gTTS(text=trans, lang=Targlang, slow=False)
+                time.sleep(0.5)
+                speak.save('trans.mp3')
+                time.sleep(0.5)
+                playsound('trans.mp3')
+                time.sleep(0.5)
+                os.remove("trans.mp3")
 
-features_num = ['fixed acidity','volatile acidity', 'citric acid',
-                'residual sugar','chlorides','free sulfur dioxide','total sulfur dioxide',
-                'density','pH','sulphates', 'alcohol']
-result= st.button('Dự đoán ')
-sample=[ fixed_acidity,volatile_acidity, citric_acid,
-         residual_sugar,chlorides,free_sulfur_dioxide,total_sulfur_dioxide, density,pH,sulphates, alcohol]
-sample_df=pd.DataFrame([sample], columns=features_num)
+if IPtype == 'Microphone':
+    col1,col3 = st.columns(2)
+    with col1:
+        option1 = st.selectbox("Your Language",Lang)
+    with col3:
+        option3 = st.selectbox("Target Language",Lang)
+    Yourlang = Code[Lang.index(option1)]
+    Targlang = Code[Lang.index(option3)]
 
-model = pickle.load(open("finalized_model.sav","rb"))
-if result:
-    st.write('Chất lượng của rượu vang là:  ' , str(model.predict(sample_df)))
+    button=st.button('START')
+    if button :
+        r = sr.Recognizer()
+        with sr.Microphone() as mic:
+            r.adjust_for_ambient_noise(mic, duration=0.5)
+            audio = r.listen(mic)
+        try:
+            text = r.recognize_google(audio, language=Yourlang)
+            translator = Translator()
+            translation = translator.translate(text, dest=Targlang)
+            trans = translation.text
+            col1, col2 = st.columns(2)
+            with col1:
+                st.text_area('Text recoded', trans, height=8, max_chars=None, key=None)
+            with col2:
+                st.text_area('Translated', trans, height=8, max_chars=None, key=None)
+            #text to speech
+            speak = gTTS(text=trans, lang=Targlang, slow=False)
+            speak.save('trans.mp3')
+            playsound('trans.mp3')
+            os.remove("trans.mp3")
+        except sr.UnknownValueError:
+            st.write("Cannot receive your voice")
+        except sr.RequestError as e:
+            st.write("Could not request results from Speech Recognition service; {0}".format(e))
 
+if IPtype == 'Audio File':
+    col1,col3 = st.columns(2)
+    with col1:
+        option1 = st.selectbox("Your Language",Lang)
+    with col3:
+        option3 = st.selectbox("Target Language",Lang)
+    butt1=st.button('Translate')
+    Yourlang = Code[Lang.index(option1)]
+    Targlang = Code[Lang.index(option3)]
 
-st.info('Bài tập lớn Trí tuệ nhân tạo - Trần Ngoan')
-st.info('Mssv: K185480106014 ')
-st.info(' Email:tvngoan343@gmail.com')
+    uploaded_file = st.file_uploader("upload", type=['wav'],accept_multiple_files=False)
+    if butt1:
+        if uploaded_file is not None:
+            audio_bytes = uploaded_file.read()
+            st.audio(audio_bytes, format='audio / wav')
+        #speech to text
+        st.write(uploaded_file.read())
+        st.write(uploaded_file.name)
+        file_name = uploaded_file.name#'dst.wav'
+        v = sr.Recognizer()
+        with sr.AudioFile(uploaded_file.name) as source:
+            audio_data = v.record(source)
+        in_text = v.recognize_google(audio_data)
+        #translate
+        translator = Translator()
+        translation = translator.translate(in_text, dest=Targlang)
+        trans = translation.text
+        # write
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('File to text')
+            st.success(in_text)
+        with col2:
+            st.write('Translated')
+            st.success(trans)
+
